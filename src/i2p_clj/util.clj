c38445bbafc8828c2c65ed930f12a92ced423090
Justin Bishop <mail@dissoc.me>
2025-01-06 03:51:13 -0600

    change bindings to use with-open

--- src/i2p_clj/util.clj
+++ src/i2p_clj/util.clj
@@ -4,19 +4,19 @@
            [net.i2p.client I2PClientFactory]))
 
 (defn to-transit [data]
-  (let [out (ByteArrayOutputStream. 4096)
-        writer (transit/writer out :json)]
-    (transit/write writer data)
-    out))
+  (with-open [out (ByteArrayOutputStream. 4096)]
+    (let [writer (transit/writer out :json)]
+      (transit/write writer data)
+      out)))
 
 (defn from-transit [t]
-  (let [in     (ByteArrayInputStream. (if (bytes? t)
-                                        t
-                                        (.toByteArray t)))
-        reader (transit/reader in :json)]
-    (transit/read reader)))
+  (with-open [in (ByteArrayInputStream. (if (bytes? t)
+                                          t
+                                          (.toByteArray t)))]
+    (let [reader (transit/reader in :json)]
+      (transit/read reader))))
 
 (defn create-destination []
-  (let [os     (new ByteArrayOutputStream)
-        client (I2PClientFactory/createClient)]
-    (.toBase64 (.createDestination client os))))
+  (with-open [os (new ByteArrayOutputStream)]
+    (let [client (I2PClientFactory/createClient)]
+      (.toBase64 (.createDestination client os)))))
